03b22bec427c8862d6270fd12d152db290eac835
Cesar Herrera <cesarherr321@gmail.com>
2023-11-16 08:53:01 -0300

    Update cors  to all

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins '*'
 
     resource '/api/*',
       headers: :any,
@@ -10,4 +10,3 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       expose: ['Access-Control-Allow-Origin']
   end
 end
-
